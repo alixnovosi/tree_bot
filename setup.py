936de8ae77456318ce9a498cc61771ea4ae1f5d1
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-11-06 23:46:58 -0800

    init commit, working

---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, "VERSION"), encoding="utf-8") as f:
+    VERSION = f.read().strip()
+
+setup(author="Andrew Michaud",
+      author_email="bots+tree@mail.andrewmichaud.com",
+
+      entry_points={
+          "console_scripts": ["tree_bot = tree_bot.__main__:main"]
+      },
+
+      install_requires=["botskeleton>=1.0.4", "treegen"],
+
+      license="BSD3",
+
+      name="tree_bot",
+
+      packages=find_packages(),
+
+      # Project"s main homepage
+      url="https://github.com/andrewmichaud/tree_bot",
+
+      version=VERSION)
---
+++ tree_bot/__main__.py
@@ -0,0 +1,48 @@
+"""Main class for bot."""
+
+import random
+import sys
+import time
+from datetime import datetime
+from os import path
+
+import botskeleton
+import treegen
+
+
+# Delay between tweets in seconds.
+DELAY = 300
+
+if __name__ == "__main__":
+    HERE = path.abspath(path.dirname(__file__))
+    SECRETS_DIR = path.join(HERE, "SECRETS")
+    BOT_SKELETON = botskeleton.BotSkeleton(SECRETS_DIR, bot_name="treegen_bot", delay=DELAY)
+
+    LOG = botskeleton.set_up_logging()
+
+    while True:
+        IMAGE_PATH = path.join(HERE, f"test-{datetime.now()}.png")
+
+        # Pick what we're tweeting.
+
+        # Black and white/color switch.
+        LOG.info("Picking tree type.")
+        if random.choice(range(5)) < 1:
+            LOG.info("Black and white tree.")
+            tree_info = treegen.TreeInfo(colors=False, extra_branching=False,
+                                          image_path=IMAGE_PATH)
+
+        else:
+            LOG.info("Colorful tree.")
+            tree_info = treegen.TreeInfo(image_path=IMAGE_PATH)
+
+            LOG.info(f"Season of tree is {tree_info.season}")
+
+        treegen.draw(tree_info)
+
+        LOG.info("Sending out the tree.")
+        status = BOT_SKELETON.send_with_one_media(f"Here's a tree!", IMAGE_PATH)
+
+        BOT_SKELETON.nap()
+
+        break
