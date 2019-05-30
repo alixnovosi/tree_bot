0838d2125c41da42c161a949c6aec3310de776b2
Andrew Michaud <dev@drew.life>
2019-05-30 15:43:55 -0700

    repin...

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["tree_bot = tree_bot.__main__:main"]
       },
 
-      install_requires=["botskeleton>=3.2.4", "treegen"],
+      install_requires=["botskeleton>=3.3.3", "treegen"],
 
       license="BSD3",
 
